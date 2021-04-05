398485efaa8ea21ba514114500189415137c283b
ariqamuharam <ariqamuharam@gmail.com>
2021-04-05 20:06:07 +0700

    hapus middleware supaya halaman member bisa akses

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 Route::name('bageur.')->group(function () {
 	Route::group(['prefix' => 'bageur/v1'], function () {
-		Route::get('indonesia/provinsi', 'bageur\indonesia\IndonesiaController@provinsi')->middleware('bgr.verify');
-		Route::get('indonesia/provinsi/{id}', 'bageur\indonesia\IndonesiaController@provinsi_detail')->middleware('bgr.verify');
+		Route::get('indonesia/provinsi', 'bageur\indonesia\IndonesiaController@provinsi');
+		Route::get('indonesia/provinsi/{id}', 'bageur\indonesia\IndonesiaController@provinsi_detail');
 	});
 });
